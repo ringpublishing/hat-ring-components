cefa0b227fbbc7ec8da1f6dd7c8c5988b7793882
Arkadiusz Fus <Arkadiusz.Fus2@ringieraxelspringer.pl>
2026-08-12 16:24:01 +0200

    5.0.0

--- src/providers/WebsiteApiProvider.ts
+++ src/providers/WebsiteApiProvider.ts
@@ -178,7 +178,8 @@ export class WebsiteApiProvider {
 
             MonitoringProvider.counter(`info.WebsitesApiProvider.call.apiCall_${queryType}`);
 
-            const response = await global.websitesApiGotClient.query(query, variables);
+            const rawRequest = await global.websitesApiGotClient.query(query, variables);
+            const response = rawRequest.body;
 
             if (response.errors || response.error) {
                 const errorMsg = response.errors?.[0]?.message || response.error?.message || 'Unknown API error';
